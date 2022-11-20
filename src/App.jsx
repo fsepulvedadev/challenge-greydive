00871c9745e7dc0bedb8b7dd0804a3910190d6c5
Francisco <panchi_nrc@hotmail.com>
2022-11-20 16:50:44 -0300

    Cambio en la url del fetch para deploy

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
   const [casos, setCasos] = useState([]);
 
   useEffect(() => {
-    fetch("./src/assets/db.json")
+    fetch("src/assets/db.json")
       .then((res) => res.json())
       .then((data) => setCasos(data));
   }, []);
